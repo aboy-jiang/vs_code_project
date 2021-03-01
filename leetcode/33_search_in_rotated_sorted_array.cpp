1357683d3ea64feedfd62ea2b4b10cee2eb4c3ea
jiangzhichao <zhichao.jiang@drcuiyutao.com>
2021-03-01 14:24:43 +0800

    33_search_in_rotated_sorted_array

--- leetcode/33_search_in_rotated_sorted_array.cpp
+++ leetcode/33_search_in_rotated_sorted_array.cpp
@@ -12,8 +12,33 @@
 #include <vector>
 using namespace std;
 
-
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        if (n == 1) return nums[0] == target ? 0 : -1;
+        int l = 0, r = n - 1, mid;
+        while (l <= r) {
+            mid = (l + r)/2;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) { // 左侧有序
+                if (nums[0] <= target && target <= nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else { // 右侧有序
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
 
 int main(int argc, char const *argv[])
 {
